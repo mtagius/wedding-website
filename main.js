d733abbc6495f57db8eb3a61ce9de2053e8fb6b7
Matt Agius <mtagius@gmail.com>
2023-04-05 21:47:02 -0400

    added basic scrolling

--- main.js
+++ main.js
@@ -16,13 +16,12 @@ function generateBackground() {
         [0, 93, 80, 93]];
     for (i = 0; i < 100; i++) {
         star = document.createElement("img");
-        star.className = "star";
         skew = randomNumber(0, 3);
         topPos = randomNumber(skews[skew][0], skews[skew][1]);
         leftPos = randomNumber(skews[skew][2], skews[skew][3]);
         star.style = 'top:' + topPos + '%;'
             + 'left:' + leftPos + '%;'
-            + 'position: absolute; z - index: -2; ';
+            + 'position: absolute; z-index: -2; ';
         if (randomNumber(0, 100) > 80) {
             star.src = "img/star.svg";
             size = randomNumber(5, randomNumber(10, 100));
